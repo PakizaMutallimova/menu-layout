9f3d0dff499f7614c22ca3574468f6cd675fa92e
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-06-15 17:08:31 +0400

    finished main menu clicking bar

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,57 +1,58 @@
 let model = {
     "categories": [
-    {
-        "id": 0,
-        "name": "BREAKFASTS"
-    },
-    {
-        "id": 1,
-        "name": "HOT MEALS"
-    },
-    {
-        "id": 2,
-        "name": "SALADS"
-    }
+        {
+            "id": 0,
+            "name": "BREAKFASTS"
+        },
+        {
+            "id": 1,
+            "name": "HOT MEALS"
+        },
+        {
+            "id": 2,
+            "name": "SALADS"
+        }
     ],
     "items": [
-    {
-        "id": 0,
-        "categoryId": 0,
-        "name": "Full English breakfast",
-        "image": "https://dyj6gt4964deb.cloudfront.net/images/9610964997582898.jpg",
-        "description": "Fried eggs, bacon, toast, cherry tomatoes",
-        "weight": "400 g",
-        "discount": "20 %",
-        "price": "6.99"
-    },
-    {
-        "id": 1,
-        "categoryId": 1,
-        "name": "Oatmeal 1",
-        "image": "https://dyj6gt4964deb.cloudfront.net/images/204313368551993.jpg",
-        "description": "Oatmeal with seasonal berries and berry syrup",
-        "weight": "120 g",
-        "discount": "10 %",
-        "price": "4.25"
-    },
-    {
-        "id": 2,
-        "categoryId": 1,
-        "name": "Oatmeal 2",
-        "image": "https://dyj6gt4964deb.cloudfront.net/images/217460674308904.jpg",
-        "description": "Oatmeal with seasonal berries and berry syrup",
-        "weight": "120 g",
-        "discount": "10 %",
-        "price": "6.25"
-    },
-    {
-        "id": 3,
-        "categoryId": 1,
-        "name": "Oatmeal 3 Salad",
-        "image": "https://dyj6gt4964deb.cloudfront.net/images/7108784455317878.jpg",
-        "description": "Oatmeal with seasonal berries and berry syrup 2",
-        "weight": "120 g 2",
-        "discount": "12 %",
-        "price": "3.25"
-    }
-]}
+        {
+            "id": 0,
+            "categoryId": 0,
+            "name": "Full English breakfast",
+            "image": "https://dyj6gt4964deb.cloudfront.net/images/9610964997582898.jpg",
+            "description": "Fried eggs, bacon, toast, cherry tomatoes",
+            "weight": "400 g",
+            "discount": "20 %",
+            "price": "6.99"
+        },
+        {
+            "id": 1,
+            "categoryId": 1,
+            "name": "Oatmeal 1",
+            "image": "https://dyj6gt4964deb.cloudfront.net/images/204313368551993.jpg",
+            "description": "Oatmeal with seasonal berries and berry syrup",
+            "weight": "120 g",
+            "discount": "10 %",
+            "price": "4.25"
+        },
+        {
+            "id": 2,
+            "categoryId": 1,
+            "name": "Oatmeal 2",
+            "image": "https://dyj6gt4964deb.cloudfront.net/images/217460674308904.jpg",
+            "description": "Oatmeal with seasonal berries and berry syrup",
+            "weight": "120 g",
+            "discount": "10 %",
+            "price": "6.25"
+        },
+        {
+            "id": 3,
+            "categoryId": 1,
+            "name": "Oatmeal 3 Salad",
+            "image": "https://dyj6gt4964deb.cloudfront.net/images/7108784455317878.jpg",
+            "description": "Oatmeal with seasonal berries and berry syrup 2",
+            "weight": "120 g 2",
+            "discount": "12 %",
+            "price": "3.25"
+        }
+    ]
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,36 @@
-// const data = require('../json/data.json')
-// import data from '../json/data.json' with { type: "json" };
-
 const menuItem = document.querySelector('.menu-items')
+const menuCategoties = document.querySelector('.menu-categories')
 const {categories, items} = model
-// console.log(items);
 
-let allDataArray = Object.keys(items).map((key) => [items[key]]);
-let allItemsArray = Object.keys(categories).map((key) => [categories[key]]);
+let itemsData = Object.keys(items).map((key) => [items[key]]);
+let categoriesData = Object.keys(categories).map((key) => [categories[key]]);
 
-
-let data = allDataArray.map((item) => {
-    console.log(item);
-    // return item
-        return renderItem(item[0]);
-    
+const dataItem = itemsData.map((item) => {
+    return renderItem(item[0]);
 });
 
-menuItem.innerHTML += data.join("")
+menuItem.innerHTML += dataItem.join("")
 
 function renderItem(item){
+    const{description, id, image, name, price, weight} = item
     return `<div class="menu-card">
         <div class="card-body">
             <div class="card-image">
-                <img src="${item.image}" alt="">
+                <img src="${image}" alt="">
             </div>
             <div class="card-content">
                 <div class="info-menu-card d-flex">
                     <div class="product-header">
-                        <span>${item.name}</span>
-                        <span>${item.weight}</span>
+                        <span>${name}</span>
+                        <span>${weight}</span>
                     </div>
-                    <p class="product-title">${item.description}</p>
+                    <p class="product-title">${description}</p>
                 </div>
                 <div class="price-place">
                     <div class="product-price d-flex">
-                        <span>${item.price}<sup>$</sup></span>
+                        <span>${price}<sup>$</sup></span>
                         
-                        <button id="${item.id}" class="btn">+</button>
+                        <button id="${id}" class="btn">+</button>
                     </div>
                 </div>
             </div>
@@ -44,8 +38,33 @@ function renderItem(item){
     </div>`;
 }
 
+const category = categoriesData.map((cat) => {
+    // console.log(cat[0]);
+    return renderCategory(cat[0])
+})
+
+function renderCategory(category) {
+    const imageItem = itemsData.map((item) => {
+        var {image} = item[0]
+        return image
+    })
+    console.log(imageItem[1]);
+    // console.log(image);
+    console.log(itemsData);
+    let {id, name} = category
+    return `
+    <div class="category" id="${id}" style="background-image: url('${imageItem[id]}');">
+        <div class="image-opacity"></div>
+        <div class="category-header">${name}</div>
+    </div>`
+}
+
+menuCategoties.innerHTML += category.join("")
 
 
 
+
+
+// const{categoryId, description, discount, id, image, name, price, weight} = item[0]
 
 
